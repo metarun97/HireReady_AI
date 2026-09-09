05d3cf017054381ba3a5f33e7d35954812e02a8f
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2026-09-09 20:38:53 +0530

    ai service added ☑️

---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,10 @@
+import { GoogleGenAI } from "@google/genai";
+
+
+// console.log(process.env.GEMINI_API_KEY);
+
+// Get response from gemini-
+const gemini = new GoogleGenAI({})
+
+export default gemini;
+
